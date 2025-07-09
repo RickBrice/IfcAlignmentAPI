cf654e4fcaea54569e61307c0ebdbdb217d52731
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2025-07-09 09:22:35 -0700

    Updates PGSuper foundation

--- PGSuper1_Foundation.py
+++ PGSuper1_Foundation.py
@@ -10,7 +10,8 @@ import bridge.api
 
 
 
-file = ifcopenshell.open("C:\\Users\\rickb\\OneDrive\\Desktop\\PGSuper1.ifc")
+#file = ifcopenshell.open("C:\\Users\\rickb\\OneDrive\\Desktop\\PGSuper1.ifc")
+file = ifcopenshell.open("C:\\Users\\bricer\\OneDrive - Washington State Department of Transportation\Desktop\\PGSuper1.ifc")
 alignment = file.by_type("IfcAlignment")[0]
 the_bridge = file.by_type("IfcBridge")[0]
 
@@ -53,7 +54,20 @@ linear_placement2 = file.createIfcLinearPlacement(
 
 pier2 = bridge.api.get_pier(the_bridge,1)
 foundation2 = bridge.api.get_foundation(the_bridge,1)
-bridge.api.create_pier(file,pier2, foundation2, 12.,3.,2., 3,20.,1.5,4., 3.0,3.0,0.75,linear_placement2)
+bridge.api.create_pier(file,
+                       pier=pier2, 
+                       foundation=foundation2, 
+                       cap_length= 12.,
+                       cap_width=3.,
+                       cap_height=1.5, 
+                       nColumns=2,
+                       column_height=17.9,
+                       column_diameter=2.,
+                       column_spacing=7.5, 
+                       footing_length=5.0,
+                       footing_width=5.0,
+                       footing_height=1.5,
+                       placement=linear_placement2)
 
 
 dir = math.radians(ifcopenshell.api.cogo.bearing2dd("N 20 16 25.49 W"))
@@ -74,4 +88,5 @@ pier3 = bridge.api.get_pier(the_bridge,2)
 foundation3 = bridge.api.get_foundation(the_bridge,2)
 bridge.api.create_stub_abutment(file,pier3, foundation3, 12.,3.,1.,10.,1.,2.,-0.25,linear_placement3)
 
-ifcopenshell.file.write(file,"C:\\Users\\rickb\\OneDrive\\Desktop\\PGSuper1_Foundation.ifc")
+#ifcopenshell.file.write(file,"C:\\Users\\rickb\\OneDrive\\Desktop\\PGSuper1_Foundation.ifc")
+ifcopenshell.file.write(file,"C:\\Users\\bricer\\OneDrive - Washington State Department of Transportation\Desktop\\PGSuper1_Foundation.ifc")
